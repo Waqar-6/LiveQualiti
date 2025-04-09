e75c84ad570a376cc4a0986b3e48838fe9cb92fa
Waqar <waqar-1@hotmail.co.uk>
2025-04-09 22:17:49 +0100

    frontend/header: made header component resposive

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Header from "@/mycomponents/header/Header";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default HomePage;

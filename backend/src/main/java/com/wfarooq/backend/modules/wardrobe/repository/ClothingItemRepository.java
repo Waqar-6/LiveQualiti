582404eec4a2557df859d029b8069be342ba49a1
Waqar <waqar-1@hotmail.co.uk>
2025-04-09 02:29:00 +0100

    frontend: clean up starter boilerplate code

--- backend/src/main/java/com/wfarooq/backend/modules/wardrobe/repository/ClothingItemRepository.java
+++ backend/src/main/java/com/wfarooq/backend/modules/wardrobe/repository/ClothingItemRepository.java
@@ -1,12 +1,19 @@
 package com.wfarooq.backend.modules.wardrobe.repository;
 
+import com.wfarooq.backend.modules.wardrobe.constants.Category;
+import com.wfarooq.backend.modules.wardrobe.constants.Color;
+import com.wfarooq.backend.modules.wardrobe.constants.Season;
 import com.wfarooq.backend.modules.wardrobe.domain.ClothingItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ClothingItemRepository extends JpaRepository<ClothingItem, UUID> {
     boolean existsByName (String name);
+    List<ClothingItem> findByColor (Color color);
+    List<ClothingItem> findBySeason (Season season);
+    List<ClothingItem> findByCategory (Category category);
 }

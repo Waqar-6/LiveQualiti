98f2f1aab90ea8f57303119e95877b6fbe5e1f54
Waqar <waqar-1@hotmail.co.uk>
2025-04-10 00:00:51 +0100

    frontend/hero: add basic resposive hero component not the best will come back and improve but okay for now

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import Header from "@/mycomponents/header/Header";
+import Hero from "@/mycomponents/hero/Hero";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 };

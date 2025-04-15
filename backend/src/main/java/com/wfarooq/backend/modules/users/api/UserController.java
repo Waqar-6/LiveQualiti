2a88943af2123656b76acf8444098402e3311c06
Waqar <waqar-1@hotmail.co.uk>
2025-04-15 19:55:37 +0100

    backend/users: add user controller with crud endpoints: get post for the moment

--- backend/src/main/java/com/wfarooq/backend/modules/users/api/UserController.java
+++ backend/src/main/java/com/wfarooq/backend/modules/users/api/UserController.java
@@ -1,4 +1,44 @@
 package com.wfarooq.backend.modules.users.api;
 
+import com.wfarooq.backend.modules.users.application.IUserService;
+import com.wfarooq.backend.modules.users.dto.request.CreateUserRequest;
+import com.wfarooq.backend.modules.users.dto.response.UserResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/api/v1/users", produces = {MediaType.APPLICATION_JSON_VALUE})
+@Validated
 public class UserController {
+
+    private final IUserService userService;
+
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser (@Valid @RequestBody CreateUserRequest request) {
+        UserResponse userResponse = userService.createUser(request);
+        return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById (@PathVariable UUID id) {
+        UserResponse userResponse = userService.getUserById(id);
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers () {
+        List<UserResponse> allUsers = userService.getAllUsers();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
 }
